2c666cc61fe45412b1c87e013de8ab75702bbac4
AfiqFadhlillah <20FTT1447@student.pb.edu.bn>
2022-04-14 22:54:52 +0800

    update index.html and vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,49 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+<div class="bg-lime-400 grid gap-4 sm:grid-cols-1">
+  <h1 class="text-3xl font-bold underline">
+    AK AFIQ FADHLILLAH
+  </h1>
+<img src="./assets/img-min.jpg" class="w-40 m-auto">
+      <ul class="text-lg my-7">
+		    <li>Phone Number: +6738893777</li>
+        <li>Email: 20FTT1447@student.pb.edu.bn</li>
+	    </ul>
+
+<h1 class="text-xl bold my-7">Education</h1>
+
+    <div class="grid grid-cols-3 gap-4">
+      <div class="bg-slate-600 text-slate-50">
+        <p>2011 - 2015</p>
+        <p>O' Level</p>
+        <p>SM Sayyidina Abu Bakar</p>
+      </div>
+
+      <div class="bg-slate-600 text-slate-50">
+        <p>2016 - 2017</p>
+        <p>Ntec in Information Technology</p>
+        <p>IBTE Sultan Saiful Rijal Campus</p>
+      </div>
+
+      <div class="bg-slate-600 text-slate-50">
+        <p class="">2017 - 2019</p>
+        <p>HNtec in Information Technology</p>
+        <p>IBTE Sultan Saiful Rijal Campus</p>
+      </div>
+    </div>
+
+    <h1 class="text-xl bold my-7">Profile</h1>
+
+    <div class="grid grid-flow-col auto-cols-max">
+      <div>01</div>
+      <div>02</div>
+      <div>03</div>
+    </div>
+
+    </div>
+
 </template>
 
 <style>
