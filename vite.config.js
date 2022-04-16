79f0d0478453634617ead2aa3924587c3fa2c83c
AfiqFadhlillah <20FTT1447@student.pb.edu.bn>
2022-04-16 09:42:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: afiqfadhlillah.github.io,
+  // base: afiqfadhlillah.github.io,
   plugins: [vue()]
 })
